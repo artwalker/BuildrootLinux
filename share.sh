3ac753a0dcd7260cc6239a438fba95f88b6a85ba
artwalker <xinxing.wang@acm.org>
2025-02-05 14:34:02 +0800

    Add the share environment parameters script.

---
+++ share.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Shared definitions for buildroot scripts
+
+QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
+MODIFIED_QEMU_DEFCONFIG=base_external/configs/qemu_defconfig
+
+DEFAULT_DEFCONFIG=${QEMU_DEFCONFIG}
+MODIFIED_DEFCONFIG=${MODIFIED_QEMU_DEFCONFIG}
+MODIFIED_DEFCONFIG_REL_BUILDROOT=../${MODIFIED_DEFCONFIG}
